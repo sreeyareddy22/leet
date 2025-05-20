5484f4469cd5f8fed404bac3e3ed54d60f3b6b4a
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-05-20 16:57:41 +0530

    Time: 4 ms (58.44%) | Memory: 50.3 MB (16.67%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,15 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int c1 = 0,c2 = 0;
         int n = nums.length;
-        List<Integer> res = new ArrayList<>();
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        Integer can1 = null, can2 = null;
         for(int i = 0;i < n;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+            if(can1 != null && nums[i] == can1)
+                c1++;
+            else if(can2 != null && nums[i] == can2)
+                c2++;
+            else if(c1 == 0){
+                can1 = nums[i];
+                c1++;
+            }
+            else if(c2 == 0){
+                can2 = nums[i];
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
         }
-        for(Map.Entry<Integer,Integer> e : hm.entrySet()){
-            if(e.getValue() > n/3)
-                res.add(e.getKey());
+        c1 = 0;
+        c2 = 0;
+        for(int i : nums){
+            if(i == can1)   
+                c1++;
+            else if(i == can2){
+                c2++;
+            }
         }
+        List<Integer> res = new ArrayList<>();
+        if(c1 > n/3)
+            res.add(can1);
+        if(c2 > n/3)
+            res.add(can2);
     return res;
     }
 }
