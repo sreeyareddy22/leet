fd7fa708bfe8c1e18761548c37e983d2a9c15a6d
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-05-20 16:28:48 +0530

    Time: 14 ms (21.03%) | Memory: 50 MB (19.69%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -4,12 +4,10 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i = 0;i < n;i++){
-            int key = nums[i];
-            hm.put(key,hm.getOrDefault(key,0)+1);
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
         for(Map.Entry<Integer,Integer> e : hm.entrySet()){
-            int val = e.getValue();
-            if(val > n/3)
+            if(e.getValue() > n/3)
                 res.add(e.getKey());
         }
     return res;
