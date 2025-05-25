090eab23d24106ef6dde536beeae1a0a1d63e3b5
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-05-26 00:58:32 +0530

    Time: 58 ms (67.07%) | Memory: 58.8 MB (37.60%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -6,25 +6,25 @@ class Solution {
             hm.put(key,hm.getOrDefault(key,0)+1);
         }
         int res = 0;
-        int cnt = 0;
+        boolean mid = false;
         for(Map.Entry<String,Integer> e : hm.entrySet()){
             String s1 = e.getKey();
             int v1 = e.getValue();
             String s2 = new StringBuilder(s1).reverse().toString();
             if(s1.equals(s2)){
                 res += 4*(v1/2);
-                cnt = Math.max(cnt,2*(v1%2));
+                if(v1%2 == 1)
+                    mid = true; 
                 continue;
             }
             if(hm.containsKey(s2)){
                 int v2 = hm.get(s2);
                 int mini = Math.min(v1,v2);
                 res += (4*mini);
-                v1 -= mini;
-                v2 -= mini;
-                hm.put(s1,v1);hm.put(s2,v2);
+                hm.put(s2,0);
             }
         }   
-    return res+cnt;
+        if(mid) res += 2;
+    return res;
     }
 }
