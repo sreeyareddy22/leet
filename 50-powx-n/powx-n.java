660a43fd5ac7c683eb5a6f16b0cf39a69d268e8d
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-05-26 16:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (53.15%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(x == 1)
+            return x;
+        double ans = 1;
+        if( n < 0){
+            n = -n;
+            x = 1/x;
+        }
+        while(n != 0){
+            if((n&1) != 0){
+                ans = ans*x;
+            }
+            x *= x;
+            n = n >>> 1;
+        }
+    return ans;
     }
 }
