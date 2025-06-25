28b22800c7e03912d1fd56c83a6ba61083c27eb9
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-06-25 13:17:37 +0530

    Time: 2 ms (98.56%) | Memory: 42.7 MB (95.32%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = "";
-        for(int i = 0;i < s.length();i++){
-            char ch = s.charAt(i);
-            if(Character.isLetter(ch))
-                str = str + Character.toLowerCase(ch);
-            if(Character.isDigit(ch))
-                str = str + ch;
-        }
-        boolean flag = isPalin(str);
-        return flag;
-    }
-    static boolean isPalin(String s){
-        int n = s.length();
-        for(int i = 0;i < s.length();i++){
-            if(s.charAt(i) != s.charAt(n-i-1))
-                return false;
+        int i = 0,j = s.length()-1;
+        while(i < j){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i)))
+                i++;
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j)))
+                j--;
+            char c1 = Character.toLowerCase(s.charAt(i));
+            char c2 = Character.toLowerCase(s.charAt(j));
+    
+            if(i <= j){
+                if(c1 != c2)
+                    return false;
+            }
+            i++;j--;
         }
     return true;
     }
