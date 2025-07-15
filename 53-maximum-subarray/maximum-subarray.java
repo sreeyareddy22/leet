c72afe35d543767b69cd10bd9df59a601e815c8c
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-07-15 20:02:12 +0530

    Time: 1 ms (99.51%) | Memory: 56.5 MB (98.71%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,23 +1,10 @@
 class Solution {
-    //kadane alogorithm - subarray with largest sum.
     public int maxSubArray(int[] nums) {
-        int maxi = Integer.MIN_VALUE;
-        boolean flag = false;
-        for(int i = 0;i < nums.length;i++){
-            maxi = Math.max(maxi,nums[i]);
-            if(nums[i] >= 0){
-                flag = true;
-            }
+        int sum = nums[0], maxi = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            sum = Math.max(sum+nums[i],nums[i]);
+            maxi = Math.max(maxi,sum);
         }
-        int maxSum = maxi;
-        int sum = nums[0];
-        for(int i = 1 ;i < nums.length;i++){
-            if(sum < 0)
-                sum = 0;
-            sum += nums[i];
-            if(maxSum < sum)  
-                maxSum = sum;
-        }
-    return flag ? maxSum : maxi ;
+        return maxi;
     }
 }
