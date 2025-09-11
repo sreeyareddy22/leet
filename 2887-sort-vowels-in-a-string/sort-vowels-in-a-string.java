2d9b1710540e7d3e5ca838f4a24ef6133c6e253c
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-09-11 11:31:14 +0530

    Time: 931 ms (6.00%) | Memory: 48.9 MB (19.94%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,23 +1,27 @@
 class Solution {
     public String sortVowels(String s) {
         String vow = "";
+        int n = s.length();
         ArrayList<Character> al = new ArrayList<>();
         String vowels = "aeiouAEIOU";
-        for(char ch : s.toCharArray()){
+        boolean[] b = new boolean[n];
+        for(int i = 0;i < n;i++){
+            char ch = s.charAt(i);
             if(vowels.indexOf(ch) != -1){
                 al.add(ch);
+                b[i] = true;
             }
         }
         Collections.sort(al);
-        int i = 0;
+        int p = 0;
         String res = "";
-        for(char ch : s.toCharArray()){
-            if(vowels.indexOf(ch) != -1){
-                res += al.get(i);
-                i++;
+        for(int i = 0;i < n;i++){
+            if(b[i]){
+                res += al.get(p);
+                p++;
             }
             else
-                res += ch;
+                res += s.charAt(i);
         }
         return res;
     }
