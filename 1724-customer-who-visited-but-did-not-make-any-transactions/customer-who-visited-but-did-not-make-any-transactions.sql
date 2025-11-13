b79b7252156a0aba4356e07a593bf5deb96c782c
sreeyareddy22 <sreeyareddy1609@gmail.com>
2025-11-13 19:24:41 +0530

    Time: 1447 ms (21.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,10 @@
+select customer_id, count(visit_id) as count_no_trans
+from Visits 
+where visit_id 
+in(
+select visit_id 
+from Visits 
+where visit_id 
+not in (select visit_id from Transactions)
+)
+group by customer_id;
